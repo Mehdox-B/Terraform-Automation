1c391aa8594059d2f919508fde918a5e7e4e26f0
Mehdox-B <mehdiben.tech@gmail.com>
2024-03-09 12:28:31 +0100

    Changing provider configuration

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
 terraform {
-  required_version = ">= 1.5.7"
-  backend "azurerm" {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+
+backend "azurerm" {
     resource_group_name  = "Hashicorp_Iac_Project"
     storage_account_name = "hashicorpstorageaccount"
     container_name       = "tfstatefile"
     key                  = "terraform.tfstate"
   }
 }
- 
 provider "azurerm" {
   features {}
 }
